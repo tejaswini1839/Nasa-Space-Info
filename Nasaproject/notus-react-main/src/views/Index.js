91ad33b2ee6e90b8f909865922420e1e2046e71e
tejaswini1839 <137172402+tejaswini1839@users.noreply.github.com>
2023-08-07 11:42:34 +0530

    Add files via upload

---
+++ Nasaproject/notus-react-main/src/views/Index.js
@@ -0,0 +1,58 @@
+/*eslint-disable*/
+import React from "react";
+import { Link } from "react-router-dom";
+import IndexNavbar from "components/Navbars/IndexNavbar.js";
+import Footer from "components/Footers/Footer.js";
+
+export default function Index() {
+  return (
+    <>
+      <IndexNavbar fixed />
+      <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
+        <div className="container mx-auto items-center flex flex-wrap">
+          <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
+            <div className="pt-32 sm:pt-0">
+              <h2 className="font-semibold text-4xl text-blueGray-600">
+                WHAT IS THE NGO ABOUT?
+              </h2>
+              <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi dolorum repellat, minus quo accusamus mollitia maxime dolores! Itaque cupiditate repudiandae ut perspiciatis dolorem qui! Ipsam temporibus ut ea suscipit accusantium?
+              </p>
+              <div className="mt-12">
+                <a
+                  href="https://www.creative-tim.com/learning-lab/tailwind/react/overview/notus?ref=nr-index"
+                  target="_blank"
+                  className="get-started text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
+                >
+                  More About NGO
+                </a>
+                <a
+                  href="https://github.com/creativetimofficial/notus-react?ref=nr-index"
+                  className="github-star ml-1 text-white font-bold px-6 py-4 rounded outline-none focus:outline-none mr-1 mb-1 bg-blueGray-700 active:bg-blueGray-600 uppercase text-sm shadow hover:shadow-lg ease-linear transition-all duration-150"
+                  target="_blank"
+                >
+                  Their Contribution
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <img
+          className="absolute top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860px"
+          src="https://images.unsplash.com/photo-1532629345422-7515f3d16bb6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+          alt="..."
+        />
+      </section>
+
+      
+
+      
+
+      
+
+      
+      <Footer />
+    </>
+  );
+}
